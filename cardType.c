edc38f6a33b8849e491685c3ffdf4b22f4be51de
smkwon <rnjss8m@sookmyung.ac.kr>
2021-11-13 21:50:32 +0900

    카드 정의 시도중

--- cardType.c
+++ cardType.c
@@ -5,7 +5,30 @@
 
 void cType_printCard(int cardType)
 {
-    
+    int card_2[N_2TO9] = {2,2,2};
+    int card_3[N_2TO9] = {3,3,3};
+    int card_4[N_2TO9] = {4,4,4};
+    int card_5[N_2TO9] = {5,5,5};
+    int card_6[N_2TO9] = {6,6,6};
+    int card_7[N_2TO9] = {7,7,7};
+    int card_8[N_2TO9] = {8,8,8};
+    int card_9[N_2TO9] = {9,9,9};
+
+    int card_10[N_TEN] = {10,10,10,10,10,10,10,10};
+
+    int card_11[N_11N] = {11};
+    int card_22[N_11N] = {22};
+    int card_33[N_11N] = {33};
+    int card_44[N_11N] = {44};
+    int card_55[N_11N] = {55};
+    int card_66[N_11N] = {66};
+
+    int card_76[N_76] = {76};
+
+    int N_0[N_ZERO] = {0,0,0,0};
+
+
+
 }
 
 int cType_getCardNum(int cardType)
--- cardType.h
+++ cardType.h
@@ -3,13 +3,13 @@
 
 #define N_2TO9          3
 #define N_TEN           8
-#define N_11N           1
+#define N_11MULTIPLE    1
 #define N_76            1
 #define N_ZERO          4
 #define N_MINUS         4
 #define N_DOUBLE        4
 
-
+/*
 #define CARDOFFSET_2TO9     0
 #define CARDOFFSET_TEN      (N_2TO9*8)                          //24
 #define CARDOFFSET_11N      (CARDOFFSET_TEN+N_TEN)              //32
@@ -19,7 +19,7 @@
 #define CARDOFFSET_DOUBLE   (CARDOFFSET_MINUS+N_MINUS)          //47
 
 #define N_TOTAL             (CARDOFFSET_DOUBLE+N_DOUBLE)        //51
-
+*/
 
 
 #define CARDNUM_DOUBLE      100
