86f95dc7f0de5c2840866eb638b711fd75e7c738
smkwon730 <44897282+smkwon730@users.noreply.github.com>
2021-11-30 16:00:36 +0900

    Final code

--- cardType.c
+++ cardType.c
@@ -1,17 +1,44 @@
 #include "cardType.h"
 #include <stdio.h>
-#include <string.h>
 
 
 
-
-void cType_printCard(int cardType)      //화면에 카드 내용을 출력하는 함수
-{
-    
+void cType_printCard(int cardType) {
+	/*
+	화면에 카드 내용을 출력하는 함수
+	*/
+	int card = cType_getCardNum(cardType);
+	if (card == CARDNUM_DOUBLE) {
+		printf("%s\t", "x2");
+	}
+	else {
+		printf("%d\t", card);
+	}
 }
 
-
-int cType_getCardNum(int cardType)      //카드를 표현하는 정수값의 숫자를 반환하는 함수
-{
-    
+int cType_getCardNum(int cardType) {
+	/*
+	카드를 표현하는 정수값의 숫자를 반환하는 함수
+	*/
+	if (cardType >= CARDOFFSET_2TO9 && cardType < CARDOFFSET_TEN) {
+		return cardType / 3 + 2;
+	}
+	else if (cardType >= CARDOFFSET_TEN && cardType < CARDOFFSET_11N) {
+		return 10;
+	}
+	else if (cardType >= CARDOFFSET_11N && cardType < CARDOFFSET_76) {
+		return 11 * (CARDOFFSET_76 - cardType);
+	}
+	else if (cardType >= CARDOFFSET_76 && cardType < CARDOFFSET_ZERO) {
+		return 76;
+	}
+	else if (cardType >= CARDOFFSET_ZERO && cardType < CARDOFFSET_MINUS) {
+		return 0;
+	}
+	else if (cardType >= CARDOFFSET_MINUS && cardType < CARDOFFSET_DOUBLE) {
+		return CARDNUM_MINUS;
+	}
+	else {
+		return CARDNUM_DOUBLE;
+	}
 }
